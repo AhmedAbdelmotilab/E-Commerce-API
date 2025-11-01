e9d10535dec12e035df5f3b2a396106f9669cfa4
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2025-11-02 00:26:49 +0200

    Add product, brand, and type entities
    Introduced Product, ProductBrand, and ProductType entity classes in the ProductModule. These classes inherit from BaseEntity<int> and define basic properties for use in the domain model.

---
+++ E-Commerce.Domain/Entities/ProductModule/Product.cs
@@ -0,0 +1,11 @@
+﻿using E_Commerce.Domain.Entities.SharedModule ;
+
+namespace E_Commerce.Domain.Entities.ProductModule ;
+
+public class Product : BaseEntity < int >
+{
+    public string Name { get ; set ; } = default! ;
+    public string Description { get ; set ; } = default! ;
+    public string PictureUrl { get ; set ; } = default! ;
+    public decimal Price { get ; set ; }
+}
