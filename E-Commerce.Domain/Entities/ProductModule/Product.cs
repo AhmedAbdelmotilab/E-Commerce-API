b3231e209c081d88dde29d7b7e71e55fbffd3452
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2025-11-02 13:15:03 +0200

    Add brand and type relationships to Product entity
    Introduced navigation properties and foreign keys for ProductBrand and ProductType in the Product entity to establish many-to-one relationships. This enhances the data model by linking products to their respective brands and types.

--- E-Commerce.Domain/Entities/ProductModule/Product.cs
+++ E-Commerce.Domain/Entities/ProductModule/Product.cs
@@ -8,4 +8,22 @@ public class Product : BaseEntity < int >
     public string Description { get ; set ; } = default! ;
     public string PictureUrl { get ; set ; } = default! ;
     public decimal Price { get ; set ; }
+
+    #region Relationships
+
+    #region Relationship Product M >-----> 1 ProductBrand
+
+    public int BrandId { get ; set ; }
+    public ProductBrand ProductBrand { get ; set ; } = default! ;
+
+    #endregion
+
+    #region Relationship Product M >-----> 1 ProductType
+
+    public int TypeId { get ; set ; }
+    public ProductType ProductType { get ; set ; } = default! ;
+
+    #endregion
+
+    #endregion
 }
