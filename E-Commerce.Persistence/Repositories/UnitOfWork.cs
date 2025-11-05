e95b960b16a16394dbb93e2a325b95aabd97f7b1
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2025-11-05 20:08:27 +0200

    Add UnitOfWork interface and implementation
    Introduces the IUnitOfWork interface in the domain layer and its implementation in the persistence layer. Provides repository access and save changes functionality for managing data operations.

---
+++ E-Commerce.Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using E_Commerce.Domain.Contracts ;
+using E_Commerce.Domain.Entities.SharedModule ;
+using E_Commerce.Persistence.Data.DbContexts ;
+
+namespace E_Commerce.Persistence.Repositories ;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly StoreDbContext _dbContext ;
+    private readonly Dictionary < Type , object > _repositories = [ ] ;
+
+    public UnitOfWork ( StoreDbContext dbContext )
+    {
+        _dbContext = dbContext ;
+    }
+
+    public async Task < int > SavChangesAsync ( ) => await _dbContext.SaveChangesAsync ( ) ;
+
+    public IGenericRepository < TEntity , TKey > GetRepository < TEntity , TKey > ( ) where TEntity : BaseEntity < TKey >
+    {
+        var EntityType = typeof ( TEntity ) ;
+        if ( _repositories.TryGetValue ( EntityType , out object ? repository ) )
+        {
+            return ( IGenericRepository < TEntity , TKey > ) repository ;
+        }
+
+        var NewRepo = new GenericRepository < TEntity , TKey > ( _dbContext ) ;
+        _repositories [ EntityType ] = NewRepo ;
+        return NewRepo ;
+    }
+}
