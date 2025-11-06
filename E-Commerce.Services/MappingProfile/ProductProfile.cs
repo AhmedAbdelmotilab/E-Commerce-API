2724e2edada3f6e140041eedebc1b4b1a85c7d40
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2025-11-06 20:08:45 +0200

    Add AutoMapper profile for product entities
    Introduces ProductProfile to map Product, ProductBrand, and ProductType entities to their respective DTOs using AutoMapper, including mapping navigational properties for brand and type names.

---
+++ E-Commerce.Services/MappingProfile/ProductProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper ;
+using E_Commerce.Domain.Entities.ProductModule ;
+using E_Commerce.Shared.DTOs.ProductDTOs ;
+
+namespace E_Commerce.Services.MappingProfile ;
+
+public class ProductProfile : Profile
+{
+    public ProductProfile ( )
+    {
+        #region Product Mapping
+
+        // 1. Map The Entity + Mapping The Navigational Proprieties Of Product Brand And Type
+        CreateMap < Product , ProductDto > ( )
+            .ForMember ( dest => dest.ProductType , opt => opt.MapFrom ( src => src.ProductType.Name ) )
+            .ForMember ( dest => dest.ProductBrand , opt => opt.MapFrom ( src => src.ProductBrand.Name ) ) ;
+
+        #endregion
+
+        #region ProductBrand Mapping
+
+        CreateMap < ProductBrand , BrandDto > ( ) ;
+
+        #endregion
+
+        #region ProductType Mapping
+
+        CreateMap < ProductType , TypeDto > ( ) ;
+
+        #endregion
+    }
+}
