79296a931306350977d77fab146cd8373e9c9c44
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2025-11-06 23:10:43 +0200

    Add product, brand, and type GET endpoints
    Implemented endpoints in ProductsController to retrieve all products, a product by ID, all brands, and all types. These endpoints use the product service to fetch data and return appropriate DTOs.

--- E-Commerce.Presentation/Controllers/ProductsController.cs
+++ E-Commerce.Presentation/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using E_Commerce.Services_Abstraction ;
+using E_Commerce.Shared.DTOs.ProductDTOs ;
 using Microsoft.AspNetCore.Mvc ;
 
 namespace E_Commerce.Presentation.Controllers ;
@@ -13,4 +14,52 @@ public class ProductsController : ControllerBase
     {
         _productService = productService ;
     }
+
+    #region GET ALL PRODUCTS
+
+    [ HttpGet ]
+    // GET : => BaseUrl/api/Products
+    public async Task < ActionResult < IEnumerable < ProductDto > > > GetAllProducts ( )
+    {
+        var Products = await _productService.GetProductsAsync ( ) ;
+        return Ok ( Products ) ;
+    }
+
+    #endregion
+
+    #region GET PRODUCT BY ID
+
+    [ HttpGet ( "{id}" ) ]
+    // GET : => BaseUrl/api/Products/1
+    public async Task < ActionResult < ProductDto > > GetProductById ( int id )
+    {
+        var Product = await _productService.GetProductByIdAsync ( id ) ;
+        return Ok ( Product ) ;
+    }
+
+    #endregion
+
+    #region GET PRODUCT BRANDS
+
+    // GET : => BaseUrl/api/Products/brands
+    [ HttpGet ( "brands" ) ]
+    public async Task < ActionResult < IEnumerable < BrandDto > > > GetProductBrands ( )
+    {
+        var Brands = await _productService.GetAllBrandsAsync ( ) ;
+        return Ok ( Brands ) ;
+    }
+
+    #endregion
+
+    #region GET PRODUCT TYPES
+
+    [ HttpGet ( "types" ) ]
+    // GET : => BaseUrl/api/Products/types
+    public async Task < ActionResult < IEnumerable < TypeDto > > > GetProductTypes ( )
+    {
+        var Types = await _productService.GetAllTypesAsync ( ) ;
+        return Ok ( Types ) ;
+    }
+
+    #endregion
 }
