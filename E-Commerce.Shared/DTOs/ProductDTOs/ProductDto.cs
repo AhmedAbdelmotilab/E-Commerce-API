4a284d0d90a6c018acf86e26fd9121fd3a92c188
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2025-11-06 18:51:08 +0200

    Add DTOs for Product, Brand, and Type
    Introduced BrandDto, ProductDto, and TypeDto classes in the ProductDTOs namespace to represent product, brand, and type data structures for shared use across the application.

---
+++ E-Commerce.Shared/DTOs/ProductDTOs/ProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace E_Commerce.Shared.DTOs.ProductDTOs ;
+
+public class ProductDto
+{
+    public int Id { get ; set ; }
+    private string Name { get ; set ; } = default! ;
+    public string Description { get ; set ; } = default! ;
+    public string PictureUrl { get ; set ; } = default! ;
+    public decimal Price { get ; set ; } = default! ;
+    public string ProductType { get ; set ; } = default! ;
+    public string ProductBrand { get ; set ; } = default! ;
+}
---
+++ E-Commerce.Shared/DTOs/ProductDTOs/TypeDto.cs
@@ -0,0 +1,7 @@
+﻿namespace E_Commerce.Shared.DTOs.ProductDTOs ;
+
+public class TypeDto
+{
+    public int Id { get ; set ; }
+    public string Name { get ; set ; } = default! ;
+}
