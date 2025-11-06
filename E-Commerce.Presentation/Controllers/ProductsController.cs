a128e20e7891c399012536f94ca9de8658133b20
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2025-11-06 22:54:45 +0200

    Add initial ProductController
    Created the ProductController class with basic ApiController and route attributes. No actions have been implemented yet.

---
+++ E-Commerce.Presentation/Controllers/ProductsController.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Mvc ;
+
+namespace E_Commerce.Presentation.Controllers ;
+
+[ ApiController ]
+[ Route ( "api/[controller]" ) ]
+public class ProductsController : ControllerBase { }
