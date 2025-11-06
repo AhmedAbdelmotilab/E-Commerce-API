168f4542437478275ce786f3f13a780b55dc92a1
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2025-11-06 20:08:52 +0200

    Add ProductService implementation
    Introduces the ProductService class implementing IProductService, providing methods to retrieve products, brands, and types using IUnitOfWork and AutoMapper.

---
+++ E-Commerce.Services/Services/ProductService.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper ;
+using E_Commerce.Domain.Contracts ;
+using E_Commerce.Domain.Entities.ProductModule ;
+using E_Commerce.Services_Abstraction ;
+using E_Commerce.Shared.DTOs.ProductDTOs ;
+
+namespace E_Commerce.Services.Services ;
+
+public class ProductService : IProductService
+{
+    private readonly IMapper _mapper ;
+    private readonly IUnitOfWork _unitOfWork ;
+
+    public ProductService ( IUnitOfWork unitOfWork , IMapper mapper )
+    {
+        _unitOfWork = unitOfWork ;
+        _mapper = mapper ;
+    }
+
+    public async Task < IEnumerable < ProductDto > > GetProductsAsync ( )
+    {
+        var Products = await _unitOfWork.GetRepository < Product , int > ( ).GetAllAsync ( ) ;
+        return _mapper.Map < IEnumerable < ProductDto > > ( Products ) ;
+    }
+
+    public async Task < ProductDto > GetProductByIdAsync ( int id )
+    {
+        var Product = await _unitOfWork.GetRepository < Product , int > ( ).GetByIdAsync ( id ) ;
+        return _mapper.Map < ProductDto > ( Product ) ;
+    }
+
+    public async Task < IEnumerable < BrandDto > > GetAllBrandsAsync ( )
+    {
+        var Brands = await _unitOfWork.GetRepository < ProductBrand , int > ( ).GetAllAsync ( ) ;
+        return _mapper.Map < IEnumerable < BrandDto > > ( Brands ) ;
+    }
+
+    public async Task < IEnumerable < TypeDto > > GetAllBrandByIdAsync ( )
+    {
+        var Types = await _unitOfWork.GetRepository < ProductType , int > ( ).GetAllAsync ( ) ;
+        return _mapper.Map < IEnumerable < TypeDto > > ( Types ) ;
+    }
+}
