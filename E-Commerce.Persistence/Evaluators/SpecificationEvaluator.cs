2818ec72917171c1f77f520a96fd5dabbb4e4134
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2025-11-09 04:18:05 -0800

    Add SpecificationEvaluator for query building
    Introduces the SpecificationEvaluator class to construct queries based on provided specifications, supporting criteria filtering and include expressions for entity relationships.

---
+++ E-Commerce.Persistence/Evaluators/SpecificationEvaluator.cs
@@ -0,0 +1,27 @@
+﻿using E_Commerce.Domain.Contracts ;
+using E_Commerce.Domain.Entities.SharedModule ;
+using Microsoft.EntityFrameworkCore ;
+
+namespace E_Commerce.Persistence.Evaluators ;
+
+internal static class SpecificationEvaluator
+{
+    public static IQueryable < TEntity > CreateQuery < TEntity , TKey > (
+        IQueryable < TEntity > entryPoint ,
+        ISpecifications < TEntity , TKey > ? specifications ) where TEntity : BaseEntity < TKey >
+    {
+        var Query = entryPoint ;
+        if ( specifications is not null && specifications.IncludeExpression.Any ( ) )
+        {
+            if ( specifications.IncludeExpression.Any ( ) )
+            {
+                Query = Query.Where ( specifications.Criteria ) ;
+            }
+
+            Query = specifications.IncludeExpression.Aggregate ( Query ,
+                ( currentQuery , includeExp ) => currentQuery.Include ( includeExp ) ) ;
+        }
+
+        return Query ;
+    }
+}
