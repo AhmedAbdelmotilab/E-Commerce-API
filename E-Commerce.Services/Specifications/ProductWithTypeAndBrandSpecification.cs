926115deff63a81489f3bad31429c877e87bb706
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2025-11-09 04:17:56 -0800

    Add ProductWithTypeAndBrandSpecification class
    Introduces a specification for products that includes related ProductType and ProductBrand entities. Provides constructors for both all products and filtering by product ID.

---
+++ E-Commerce.Services/Specifications/ProductWithTypeAndBrandSpecification.cs
@@ -0,0 +1,18 @@
+﻿using E_Commerce.Domain.Entities.ProductModule ;
+
+namespace E_Commerce.Services.Specifications ;
+
+public class ProductWithTypeAndBrandSpecification : BaseSpecification < Product , int >
+{
+    public ProductWithTypeAndBrandSpecification ( ) : base ( null )
+    {
+        AddInclude ( P => P.ProductType ) ;
+        AddInclude ( P => P.ProductBrand ) ;
+    }
+
+    public ProductWithTypeAndBrandSpecification ( int id ) : base ( P => P.Id == id )
+    {
+        AddInclude ( P => P.ProductType ) ;
+        AddInclude ( P => P.ProductBrand ) ;
+    }
+}
