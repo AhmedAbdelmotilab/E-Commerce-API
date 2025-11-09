16434e85ece088d0f6cbfc5852242669c03ba6b5
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2025-11-09 04:17:05 -0800

    Add base specification pattern interfaces and class
    Introduced ISpecifications interface and BaseSpecification abstract class to support the specification pattern for querying entities. This provides a structured way to define query criteria and include expressions for entities inheriting from BaseEntity.

---
+++ E-Commerce.Domain/Contracts/ISpecifications.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions ;
+using E_Commerce.Domain.Entities.SharedModule ;
+
+namespace E_Commerce.Domain.Contracts ;
+
+public interface ISpecifications < TEntity , TKey > where TEntity : BaseEntity < TKey >
+{
+    public ICollection < Expression < Func < TEntity , object > > > IncludeExpression { get ; }
+    public Expression < Func < TEntity , bool > > Criteria { get ; }
+}
---
+++ E-Commerce.Services/Specifications/BaseSpecification.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions ;
+using E_Commerce.Domain.Contracts ;
+using E_Commerce.Domain.Entities.SharedModule ;
+
+namespace E_Commerce.Services.Specifications ;
+
+public abstract class BaseSpecification < TEntity , TKey > : ISpecifications < TEntity , TKey > where TEntity : BaseEntity < TKey >
+{
+    public BaseSpecification ( Expression < Func < TEntity , bool > > criteriaExpression )
+    {
+        Criteria = criteriaExpression ;
+    }
+
+    public Expression < Func < TEntity , bool > > Criteria { get ; }
+    public ICollection < Expression < Func < TEntity , object > > > IncludeExpression { get ; } = [ ] ;
+
+    protected void AddInclude ( Expression < Func < TEntity , object > > expression ) => IncludeExpression.Add ( expression ) ;
+}
