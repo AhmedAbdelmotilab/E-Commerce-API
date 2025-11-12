a6021bcffe984f461083798c5308dec2e43d7221
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2025-11-12 04:10:03 +0200

    Add BasketProfile for AutoMapper configuration
    Introduces BasketProfile to map between CustomerBasket and BasketDto, as well as BasketItem and BasketItemDto, using AutoMapper. This enables easier transformation between domain entities and DTOs in the basket module.

---
+++ E-Commerce.Services/MappingProfile/BasketProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper ;
+using E_Commerce.Domain.Entities.BasketModule ;
+using E_Commerce.Shared.DTOs.BasketDTOs ;
+
+namespace E_Commerce.Services.MappingProfile ;
+
+public class BasketProfile : Profile
+{
+    public BasketProfile ( )
+    {
+        CreateMap < CustomerBasket , BasketDto > ( ).ReverseMap ( ) ;
+        CreateMap < BasketItem , BasketItemDto > ( ).ReverseMap ( ) ;
+    }
+}
