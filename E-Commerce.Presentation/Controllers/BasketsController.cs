b886aed4f7cb85ff5ed9a01f86a89498f9133235
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2025-11-12 04:13:06 +0200

    Add BasketsController with dependency injection
    Introduces BasketsController to the presentation layer, injecting IBasketService via constructor. This sets up the controller for future basket-related API endpoints.

---
+++ E-Commerce.Presentation/Controllers/BasketsController.cs
@@ -0,0 +1,16 @@
+﻿using E_Commerce.Services_Abstraction ;
+using Microsoft.AspNetCore.Mvc ;
+
+namespace E_Commerce.Presentation.Controllers ;
+
+[ ApiController ]
+[ Route ( "api/[controller]" ) ]
+public class BasketsController : ControllerBase
+{
+    private readonly IBasketService _basketService ;
+
+    public BasketsController ( IBasketService basketService )
+    {
+        _basketService = basketService ;
+    }
+}
