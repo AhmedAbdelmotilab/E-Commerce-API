3d3413cb96c5ab16295c019abfafd129a5ce2a63
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2025-11-12 04:09:55 +0200

    Add BasketService and IBasketService interface
    Introduced IBasketService interface and its implementation BasketService to handle basket operations such as get, create/update, and delete. These services use AutoMapper and a repository for data access and mapping between entities and DTOs.

---
+++ E-Commerce.Services Abstraction/IBasketService.cs
@@ -0,0 +1,10 @@
+﻿using E_Commerce.Shared.DTOs.BasketDTOs ;
+
+namespace E_Commerce.Services_Abstraction ;
+
+public interface IBasketService
+{
+    Task < BasketDto > GetBasketAsync ( string Id ) ;
+    Task < BasketDto > CreateOrUpdateAsync ( BasketDto basket ) ;
+    Task < bool > DeleteBasketAsync ( string id ) ;
+}
---
+++ E-Commerce.Services/Services/BasketService.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper ;
+using E_Commerce.Domain.Contracts ;
+using E_Commerce.Domain.Entities.BasketModule ;
+using E_Commerce.Services_Abstraction ;
+using E_Commerce.Shared.DTOs.BasketDTOs ;
+
+namespace E_Commerce.Services.Services ;
+
+public class BasketService : IBasketService
+{
+    private readonly IMapper _mapper ;
+    private readonly IBasketRepository _repository ;
+
+    public BasketService ( IBasketRepository repository , IMapper mapper )
+    {
+        _repository = repository ;
+        _mapper = mapper ;
+    }
+
+    public async Task < BasketDto > GetBasketAsync ( string Id )
+    {
+        var Basket = await _repository.GetBasketAsync ( Id ) ;
+        return _mapper.Map < CustomerBasket , BasketDto > ( Basket! ) ;
+    }
+
+    public async Task < BasketDto > CreateOrUpdateAsync ( BasketDto basket )
+    {
+        var CustomerBasket = _mapper.Map < CustomerBasket > ( basket ) ;
+        var CreatedOrUpdatedBasket = await _repository.CreateOrUpdateAsync ( CustomerBasket ) ;
+        return _mapper.Map < CustomerBasket , BasketDto > ( CreatedOrUpdatedBasket ! ) ;
+    }
+
+    public Task < bool > DeleteBasketAsync ( string id ) => _repository.DeleteBasketAsync ( id ) ;
+}
