2b8ce05288e4be72e2dd511e34b18f11f8da93f5
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2025-11-12 05:15:39 +0200

    Add basket CRUD endpoints to BasketsController
    Implemented GET, POST, and DELETE endpoints in BasketsController for retrieving, creating/updating, and deleting baskets. These endpoints interact with the basket service and use BasketDto for data transfer.

--- E-Commerce.Presentation/Controllers/BasketsController.cs
+++ E-Commerce.Presentation/Controllers/BasketsController.cs
@@ -1,4 +1,5 @@
 ﻿using E_Commerce.Services_Abstraction ;
+using E_Commerce.Shared.DTOs.BasketDTOs ;
 using Microsoft.AspNetCore.Mvc ;
 
 namespace E_Commerce.Presentation.Controllers ;
@@ -13,4 +14,37 @@ public class BasketsController : ControllerBase
     {
         _basketService = basketService ;
     }
+
+    #region Get Basket : BaseUrl/api/Baskets?id = " "
+
+    [ HttpGet ]
+    public async Task < ActionResult < BasketDto ? > > GetBasket ( string id )
+    {
+        var Basket = await _basketService.GetBasketAsync ( id ) ;
+        return Ok ( Basket ) ;
+    }
+
+    #endregion
+
+    #region POST : BaseUrl/api/Baskets
+
+    [ HttpPost ]
+    public async Task < ActionResult < BasketDto > > CreateOrUpdateBasket ( BasketDto basketDto )
+    {
+        var Basket = await _basketService.CreateOrUpdateAsync ( basketDto ) ;
+        return Ok ( Basket ) ;
+    }
+
+    #endregion
+
+    #region Delete Basket : BaseUrl/api/Baskets/{id}
+
+    [ HttpDelete ( "{id}" ) ]
+    public async Task < ActionResult < bool > > DeleteBasket ( string id )
+    {
+        var Result = await _basketService.DeleteBasketAsync ( id ) ;
+        return Ok ( Result ) ;
+    }
+
+    #endregion
 }
