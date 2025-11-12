e0ed99d117465b08858eba80aa846c39e4a9359c
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2025-11-12 04:01:04 +0200

    Add basket repository interface and implementation
    Introduced IBasketRepository interface in the domain layer and its Redis-based implementation in the persistence layer. Provides methods to get, create/update, and delete customer baskets.

---
+++ E-Commerce.Domain/Contracts/IBasketRepository.cs
@@ -0,0 +1,10 @@
+﻿using E_Commerce.Domain.Entities.BasketModule ;
+
+namespace E_Commerce.Domain.Contracts ;
+
+public interface IBasketRepository
+{
+    Task < CustomerBasket ? > GetBasketAsync ( string basketId ) ;
+    Task < CustomerBasket ? > CreateOrUpdateAsync ( CustomerBasket basket , TimeSpan timeToLive = default ) ;
+    Task < bool > DeleteBasketAsync ( string basketId ) ;
+}
---
+++ E-Commerce.Persistence/Repositories/BasketRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json ;
+using E_Commerce.Domain.Contracts ;
+using E_Commerce.Domain.Entities.BasketModule ;
+using StackExchange.Redis ;
+
+namespace E_Commerce.Persistence.Repositories ;
+
+public class BasketRepository : IBasketRepository
+{
+    private readonly IDatabase _database ;
+
+    public BasketRepository ( IConnectionMultiplexer connection )
+    {
+        _database = connection.GetDatabase ( ) ;
+    }
+
+    public async Task < CustomerBasket ? > CreateOrUpdateAsync ( CustomerBasket basket , TimeSpan timeToLive = default )
+    {
+        var JsonBasket = JsonSerializer.Serialize ( basket ) ;
+        var IsCreatedOrUpdated = await
+            _database.StringSetAsync ( basket.Id , JsonBasket , ( timeToLive == default ) ? TimeSpan.FromDays ( 7 ) : timeToLive ) ;
+        if ( IsCreatedOrUpdated )
+        {
+            return await GetBasketAsync ( basket.Id ) ;
+        }
+        else
+        {
+            return null ;
+        }
+    }
+
+    public async Task < CustomerBasket ? > GetBasketAsync ( string basketId )
+    {
+        var Basket = await _database.StringGetAsync ( basketId ) ;
+        if ( Basket.IsNullOrEmpty ) return null ;
+        return JsonSerializer.Deserialize < CustomerBasket > ( Basket! ) ;
+    }
+
+    public async Task < bool > DeleteBasketAsync ( string basketId ) => await _database.KeyDeleteAsync ( basketId ) ;
+}
