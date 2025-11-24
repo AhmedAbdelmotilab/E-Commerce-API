bc1735bba909fc84944c8dd02942fadc988d9a50
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2025-11-24 11:14:23 +0200

    Add JWT token generation to AuthenticationService
    Introduced a private CreateTokenAsync method to generate JWT tokens for authenticated users. Injected IConfiguration to access JWT settings such as SecretKey, Issuer, and Audience. This enhances authentication by enabling token-based security.

--- E-Commerce.Services/Services/AuthenticationService.cs
+++ E-Commerce.Services/Services/AuthenticationService.cs
@@ -1,18 +1,25 @@
-﻿using E_Commerce.Domain.Entities.IdentityModule ;
+﻿using System.IdentityModel.Tokens.Jwt ;
+using System.Security.Claims ;
+using System.Text ;
+using E_Commerce.Domain.Entities.IdentityModule ;
 using E_Commerce.Services_Abstraction ;
 using E_Commerce.Shared.CommonResult ;
 using E_Commerce.Shared.DTOs.IdentityDTOs ;
 using Microsoft.AspNetCore.Identity ;
+using Microsoft.Extensions.Configuration ;
+using Microsoft.IdentityModel.Tokens ;
 
 namespace E_Commerce.Services.Services ;
 
 public class AuthenticationService : IAuthenticationService
 {
+    private readonly IConfiguration _configuration ;
     private readonly UserManager < ApplicationUser > _userManager ;
 
-    public AuthenticationService ( UserManager < ApplicationUser > userManager )
+    public AuthenticationService ( UserManager < ApplicationUser > userManager , IConfiguration configuration )
     {
         _userManager = userManager ;
+        _configuration = configuration ;
     }
 
     public async Task < Result < UserDto > > LoginAsync ( LoginDto loginDto )
@@ -51,4 +58,34 @@ public class AuthenticationService : IAuthenticationService
             return IdentityResult.Errors.Select ( E => Error.Validation ( E.Code , E.Description ) ).ToList ( ) ;
         }
     }
+
+    #region Method For Generate Token
+
+    private async Task < string > CreateTokenAsync ( ApplicationUser user )
+    {
+        var Claims = new List < Claim > ( )
+        {
+            new Claim ( JwtRegisteredClaimNames.Email , user.Email! ) ,
+            new Claim ( JwtRegisteredClaimNames.Name , user.UserName! ) ,
+        } ;
+        var Roles = await _userManager.GetRolesAsync ( user ) ;
+        foreach ( var Role in Roles )
+        {
+            Claims.Add ( new Claim ( ClaimTypes.Role , Role ) ) ;
+        }
+
+        var SecretKey = _configuration [ "JWTOptions:SecretKey" ] ;
+        var Key = new SymmetricSecurityKey ( Encoding.UTF8.GetBytes ( SecretKey! ) ) ;
+        var Cred = new SigningCredentials ( Key , SecurityAlgorithms.HmacSha256 ) ;
+        var Token = new JwtSecurityToken (
+            issuer : _configuration [ "JWTOptions:Issuer" ] ,
+            audience : _configuration [ "JWTOptions:Audience" ] ,
+            claims : Claims ,
+            expires : DateTime.UtcNow.AddHours ( 1 ) ,
+            signingCredentials : Cred
+        ) ;
+        return new JwtSecurityTokenHandler ( ).WriteToken ( Token ) ;
+    }
+
+    #endregion
 }
