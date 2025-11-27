4ce5f6f03dd0e7ee75ba9198db87d7b4c6a0ff3a
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2025-11-27 20:21:55 +0200

    Add entity configurations for order-related models
    Introduced DeliveryMethodConfiguration, OrderConfiguration, and OrderItemConfiguration classes to define property constraints and precision for order-related entities in the persistence layer. These configurations set precision for price fields and maximum lengths for string properties, improving data integrity and consistency.

---
+++ E-Commerce.Persistence/Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using E_Commerce.Domain.Entities.OrderModule ;
+using Microsoft.EntityFrameworkCore ;
+using Microsoft.EntityFrameworkCore.Metadata.Builders ;
+
+namespace E_Commerce.Persistence.Data.Configurations ;
+
+public class OrderConfiguration : IEntityTypeConfiguration < Order >
+{
+    public void Configure ( EntityTypeBuilder < Order > builder )
+    {
+        builder.Property ( X => X.SubTotal ).HasPrecision ( 8 , 2 ) ;
+        builder.OwnsOne ( X => X.Address ,
+            OEntity =>
+            {
+                OEntity.Property ( O => O.FirstName ).HasMaxLength ( 50 ) ;
+                OEntity.Property ( O => O.LastName ).HasMaxLength ( 50 ) ;
+                OEntity.Property ( O => O.Street ).HasMaxLength ( 50 ) ;
+                OEntity.Property ( O => O.City ).HasMaxLength ( 50 ) ;
+                OEntity.Property ( O => O.Country ).HasMaxLength ( 50 ) ;
+            }
+        ) ;
+    }
+}
