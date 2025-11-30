06da917e652d56ffcb68535127ffae7b8a0165a6
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2025-11-30 14:57:51 +0200

    Add delivery method DTO and expand order service interface
    Introduced DeliveryMethodDto to represent delivery methods. Extended IOrderService with methods to get all orders, fetch delivery methods, and retrieve an order by ID for a given email.

--- E-Commerce.Services Abstraction/IOrderService.cs
+++ E-Commerce.Services Abstraction/IOrderService.cs
@@ -6,4 +6,7 @@ namespace E_Commerce.Services_Abstraction ;
 public interface IOrderService
 {
     Task < Result < OrderToReturnDto > > CreateOrderAsync ( OrderDto orderDto , string Email ) ;
+    Task < Result < IEnumerable < OrderToReturnDto > > > GetAllOrdersAsync ( string Email ) ;
+    Task < Result < IEnumerable < DeliveryMethodDto > > > GetDeliveryMethodsAsync ( string Email ) ;
+    Task < Result < OrderToReturnDto > > GetOrderByIdAsync ( Guid id , string Email ) ;
 }
