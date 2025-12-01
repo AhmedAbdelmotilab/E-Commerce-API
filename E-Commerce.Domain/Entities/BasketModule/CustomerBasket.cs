605928c73b0d851eb685a99b6a133e1333b3ac44
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2025-12-01 02:11:45 +0200

    Add payment and shipping fields to basket models
    Added PaymentIntentId, ClientSecret, DeliveryMethodId, and ShippingPrice properties to CustomerBasket and BasketDto to support payment processing and shipping information in the basket module.

--- E-Commerce.Domain/Entities/BasketModule/CustomerBasket.cs
+++ E-Commerce.Domain/Entities/BasketModule/CustomerBasket.cs
@@ -4,4 +4,8 @@ public class CustomerBasket
 {
     public string Id { get ; set ; } = default! ;
     public ICollection < BasketItem > Items { get ; set ; } = [ ] ;
+    public string ? PaymentIntentId { get ; set ; }
+    public string ? ClientSecret { get ; set ; }
+    public int ? DeliveryMethodId { get ; set ; }
+    public decimal ShippingPrice { get ; set ; }
 }
